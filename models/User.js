8d20a0964fda30b395ea284ac242b1330aac7552
stfucinoo-lgtm <stfucinoo@gmail.com>
2026-07-15 22:24:49 +0100

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,7 +1,6 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/db');
-const bcrypt = require('bcrypt');
-
+const bcrypt = require('bcryptjs');
 const User = sequelize.define('User', {
   id: {
     type: DataTypes.INTEGER,
