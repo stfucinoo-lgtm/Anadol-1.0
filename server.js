05f57e11a45b235a41b82233c3e4200f288f75c1
stfucinoo-lgtm <stfucinoo@gmail.com>
2026-07-15 21:32:20 +0100

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,6 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-
 // 2. دالة آمنة لتحميل المسارات
 function safeMountRoute(routePath, moduleName) {
     try {
